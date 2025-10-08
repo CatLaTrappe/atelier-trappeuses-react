87cd8252001f0d951166f670c2d525e1ce4ee7e7
CatLaTrappe <cathynaud78@gmail.com>
2025-10-08 06:23:06 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import AtelierTextes from './modules/AtelierTextes.jsx'
-import './styles.css'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<AtelierTextes />)
